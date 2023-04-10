da9b203d6ae6f2316c0b20e5638cc4765e94f351
Антисептик <@DESKTOP-KTN6EI8>
2023-04-10 15:31:04 +0300

    12

--- grafics/Form1.Designer.cs
+++ grafics/Form1.Designer.cs
@@ -32,6 +32,11 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -71,12 +76,68 @@
             this.button3.TabIndex = 3;
             this.button3.Text = "круги";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(838, 102);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(125, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "оси";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(838, 218);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(125, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "косинус";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(838, 189);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(125, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "гипербола";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(838, 131);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(125, 23);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "парабола";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(838, 160);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(125, 23);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "куб порабола";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(995, 486);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -94,5 +155,10 @@
         private Button button1;
         private Button button2;
         private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
     }
 }
--- grafics/Form1.cs
+++ grafics/Form1.cs
@@ -61,5 +61,117 @@ namespace grafics
             }
             pictureBox1.Image = image;
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Pen pen1 = new Pen(Color.Black);
+            Pen pen2 = new Pen(Color.Red);
+            Pen pen3 = new Pen(Color.Blue);
+            Pen pen4 = new Pen(Color.Green);
+            Random rnd = new Random();
+            int x;
+            Graphics graphics = pictureBox1.CreateGraphics();
+            graphics.Clear(Color.White);
+            for(int i = 0; i<=20;i++)
+            {
+                x = rnd.Next(5, 100);
+                graphics.DrawEllipse(pen1, rnd.Next(-100,800), rnd.Next(-100, 400), x, x);
+                x = rnd.Next(5, 100);
+                graphics.DrawEllipse(pen2, rnd.Next(-100, 800), rnd.Next(-100, 400), x, x);
+                x = rnd.Next(5, 100);
+                graphics.DrawEllipse(pen3, rnd.Next(-100, 800), rnd.Next(-100, 400), x, x);
+                x = rnd.Next(5, 100);
+                graphics.DrawEllipse(pen4, rnd.Next(-100, 800), rnd.Next(-100, 400), x, x);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Graphics graphics = pictureBox1.CreateGraphics();
+            Pen pen1 = new Pen(Color.Black);
+            graphics.Clear(Color.White);
+            Point[] pointy = new Point[2] { new Point(400,0) , new Point(400,400)};
+            Point[] pointx = new Point[2] { new Point(0, 200), new Point(800, 200) };
+            graphics.DrawLines(pen1,pointy);
+            graphics.DrawLines(pen1, pointx);
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Graphics graphics = pictureBox1.CreateGraphics();
+            Pen pen1 = new Pen(Color.Black);
+            graphics.Clear(Color.White);
+            Point[] points = new Point[800];
+            int y;
+            for (int x = -400, i = 0; x < 400; x++,i++)
+            {
+                y = -(int)(Math.Sin((double)x / 40 ) * 40 ) +200 ;
+                points[i] = new Point(x + 400, y);
+            }
+            graphics.DrawLines(pen1, points);
+            Point[] pointy = new Point[2] { new Point(400, 0), new Point(400, 400) };
+            Point[] pointx = new Point[2] { new Point(0, 200), new Point(800, 200) };
+            graphics.DrawLines(pen1, pointy);
+            graphics.DrawLines(pen1, pointx);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Graphics graphics = pictureBox1.CreateGraphics();
+            graphics.Clear(Color.White);
+            Pen pen1 = new Pen(Color.Black, 1f);
+            Point[] points = new Point[800];
+            int y;
+            for (int x = -400, i = 0; x < 400; x++, i++)
+            {
+                y = -(x*x/80) + 200;
+                points[i] = new Point(x + 400, y);
+            }
+            graphics.DrawLines(pen1, points);
+            Point[] pointy = new Point[2] { new Point(400, 0), new Point(400, 400) };
+            Point[] pointx = new Point[2] { new Point(0, 200), new Point(800, 200) };
+            graphics.DrawLines(pen1, pointy);
+            graphics.DrawLines(pen1, pointx);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Graphics graphics = pictureBox1.CreateGraphics();
+            graphics.Clear(Color.White);
+            Pen pen1 = new Pen(Color.Black, 1f);
+            Point[] points = new Point[800];
+            int y;
+            for (int x = -400, i = 0; x < 400; x++, i++)
+            {
+                y = -(x * x * x / 1200) + 200;
+                points[i] = new Point(x + 400, y);
+            }
+            graphics.DrawLines(pen1, points);
+            Point[] pointy = new Point[2] { new Point(400, 0), new Point(400, 400) };
+            Point[] pointx = new Point[2] { new Point(0, 200), new Point(800, 200) };
+            graphics.DrawLines(pen1, pointy);
+            graphics.DrawLines(pen1, pointx);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Graphics graphics = pictureBox1.CreateGraphics();
+            graphics.Clear(Color.White);
+            Pen pen1 = new Pen(Color.Black, 1f);
+            Point[] points = new Point[800];
+            int y;
+            for (int x = -400, i = 0; x < 401; x++, i++)
+            {
+                if (x == 0) x++;
+                y = (int)(-(1/((double)x/400)) + 200);
+                points[i] = new Point(x + 400, y);
+            }
+            graphics.DrawLines(pen1, points);
+            Point[] pointy = new Point[2] { new Point(400, 0), new Point(400, 400) };
+            Point[] pointx = new Point[2] { new Point(0, 200), new Point(800, 200) };
+            graphics.DrawLines(pen1, pointy);
+            graphics.DrawLines(pen1, pointx);
+        }
     }
 }
